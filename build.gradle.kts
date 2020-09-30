ac1899d3c9764a142939054325a475824fb14024
Richie Ward <RichieS@GMail.com>
2020-09-30 20:15:23 +0100

    Remove spring plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.lang.System.getenv
 
 plugins {
-	id("org.springframework.boot") version "2.3.4.RELEASE"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.3.72"
-	kotlin("plugin.spring") version "1.3.72"
 	`maven-publish`
 }
 
@@ -18,7 +15,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter:2.3.4.RELEASE")
 	implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.33")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
